25c690b89795ef80f606a127336cff1a15ca37e2
tox-in <habimanat78@gmail.com>
2025-07-21 11:31:13 +0200

    entity, repository and service definition

---
+++ src/main/java/com/demo/simplejpa/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.demo.simplejpa.repository;
+
+import com.demo.simplejpa.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
---
+++ src/main/java/com/demo/simplejpa/service/UserService.java
@@ -0,0 +1,29 @@
+package com.demo.simplejpa.service;
+
+import com.demo.simplejpa.model.User;
+import com.demo.simplejpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
