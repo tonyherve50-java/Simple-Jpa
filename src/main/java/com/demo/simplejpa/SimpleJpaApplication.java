c68a678752d3857245ddd60f7ab920de85c852bb
tox-in <habimanat78@gmail.com>
2025-07-21 10:49:37 +0200

    Project setup

---
+++ src/main/java/com/demo/simplejpa/SimpleJpaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.simplejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/simplejpa/SimpleJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.simplejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
