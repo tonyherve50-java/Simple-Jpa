383e83e58310bf4165d16b23bd7653a5201623f4
tox-in <habimanat78@gmail.com>
2025-07-21 11:58:12 +0200

    controller definition

---
+++ src/main/java/com/demo/simplejpa/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.demo.simplejpa.controller;
+
+import com.demo.simplejpa.model.User;
+import com.demo.simplejpa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public Iterable<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUserById(id);
+    }
+}
